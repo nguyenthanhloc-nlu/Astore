f462e6ba470b9ff0fab7108455a2e6092abb2a9f
nguyenthanhloc-nlu <19130125@st.hcmuaf.edu.vn>
2021-12-30 19:47:54 +0700

    first

---
+++ src/main/java/com/astore/controller/admin/dashboard/DashboardController.java
@@ -0,0 +1,19 @@
+package com.astore.controller.admin.dashboard;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+@WebServlet(name = "DashboardController", value = "/DashboardController")
+public class DashboardController extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
---
+++ src/main/java/com/astore/model/Product.java
@@ -0,0 +1,221 @@
+package com.astore.model;
+
+import java.util.List;
+
+public class Product {
+    private int id;
+    private int subCategoryId;
+    private String name;
+    private int colorId;
+    private String colorName;
+    private String colorHex;
+    private double price;
+    private String rom;
+    private String ram;
+    private String sizeScreen;
+    private String screenResolution; // độ phân giải
+    private String frontCamera;
+    private String backCamera;
+    private double saleRate;
+    private String productDetail;
+    private List<String> listPhotoUrl;
+    private List<String> listProductDetail;
+    private String createdAt;
+
+
+
+
+    public Product() {
+    }
+
+    public Product(int id, int subCategoryId, String name, int colorId, String colorName, String colorHex, double price, String rom, String ram, String sizeScreen, String screenResolution, String frontCamera, String backCamera, double saleRate, String productDetail, List<String> listPhotoUrl, List<String> listProductDetail) {
+        this.id = id;
+        this.subCategoryId = subCategoryId;
+        this.name = name;
+        this.colorId = colorId;
+        this.colorName = colorName;
+        this.colorHex = colorHex;
+        this.price = price;
+        this.rom = rom;
+        this.ram = ram;
+        this.sizeScreen = sizeScreen;
+        this.screenResolution = screenResolution;
+        this.frontCamera = frontCamera;
+        this.backCamera = backCamera;
+        this.saleRate = saleRate;
+        this.productDetail = productDetail;
+        this.listPhotoUrl = listPhotoUrl;
+        this.listProductDetail = listProductDetail;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getSubCategoryID() {
+        return subCategoryId;
+    }
+
+    public void setSubCategoryId(int subCategoryId) {
+        this.subCategoryId = subCategoryId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getSubCategoryId() {
+        return subCategoryId;
+    }
+
+    public int getColorId() {
+        return colorId;
+    }
+
+    public void setColorId(int colorId) {
+        this.colorId = colorId;
+    }
+
+    public String getColorHex() {
+        return colorHex;
+    }
+
+    public void setColorHex(String colorHex) {
+        this.colorHex = colorHex;
+    }
+
+    public String getColorName() {
+        return colorName;
+    }
+
+    public void setColorName(String colorName) {
+        this.colorName = colorName;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getRom() {
+        return rom;
+    }
+
+    public void setRom(String rom) {
+        this.rom = rom;
+    }
+
+    public String getRam() {
+        return ram;
+    }
+
+    public void setRam(String ram) {
+        this.ram = ram;
+    }
+
+    public String getSizeScreen() {
+        return sizeScreen;
+    }
+
+    public void setSizeScreen(String sizeScreen) {
+        this.sizeScreen = sizeScreen;
+    }
+
+    public String getScreenResolution() {
+        return screenResolution;
+    }
+
+    public void setScreenResolution(String screenResolution) {
+        this.screenResolution = screenResolution;
+    }
+
+    public String getFrontCamera() {
+        return frontCamera;
+    }
+
+    public void setFrontCamera(String frontCamera) {
+        this.frontCamera = frontCamera;
+    }
+
+    public String getBackCamera() {
+        return backCamera;
+    }
+
+    public void setBackCamera(String backCamera) {
+        this.backCamera = backCamera;
+    }
+
+    public double getSaleRate() {
+        return saleRate;
+    }
+
+    public void setSaleRate(double saleRate) {
+        this.saleRate = saleRate;
+    }
+
+    public String getProductDetail() {
+        return productDetail;
+    }
+
+    public void setProductDetail(String productDetail) {
+        this.productDetail = productDetail;
+    }
+
+    public List<String> getListPhotoUrl() {
+        return listPhotoUrl;
+    }
+
+    public void setListPhotoUrl(List<String> listPhotoUrl) {
+        this.listPhotoUrl = listPhotoUrl;
+    }
+
+    public List<String> getListProductDetail() {
+        return listProductDetail;
+    }
+
+    public void setListProductDetail(List<String> listProductDetail) {
+        this.listProductDetail = listProductDetail;
+    }
+
+    public String getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(String createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id=" + id +
+                ", dongSpID=" + subCategoryId +
+                ", name='" + name + '\'' +
+                ", colorId=" + colorId +
+                ", color='" + colorName + '\'' +
+                ", price=" + price +
+                ", rom='" + rom + '\'' +
+                ", ram='" + ram + '\'' +
+                ", sizeScreen='" + sizeScreen + '\'' +
+                ", screenResolution='" + screenResolution + '\'' +
+                ", frontCamera='" + frontCamera + '\'' +
+                ", backCamera='" + backCamera + '\'' +
+                ", saleRate=" + saleRate +
+                ", productDetail='" + productDetail + '\'' +
+                ", listPhotoUrl=" + listPhotoUrl +
+                ", listProductDetail=" + listProductDetail +
+                ", createdAt='" + createdAt + '\'' +
+                '}';
+    }
+}
