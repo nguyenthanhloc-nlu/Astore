18930cda3cbbc821cfabefceb2a3eb0f0ac93229
Jonathan20082001 <19130205@st.hcmuaf.edu.vn>
2022-01-14 10:49:48 +0700

    create file watch controller

--- src/main/java/com/astore/controller/client/Watch.java
+++ src/main/java/com/astore/controller/client/Watch.java
@@ -3,25 +3,28 @@ package com.astore.controller.client;
 import com.astore.model.Product;
 import com.astore.services.implement.ProductServices;
 
-import javax.servlet.*;
-import javax.servlet.http.*;
-import javax.servlet.annotation.*;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.List;
 
-@WebServlet(name = "Watch", value = "/Watch")
+@WebServlet(name = "Watch", value = "/Product-watch")
 public class Watch extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         ProductServices ps = new ProductServices();
-        List<Product> watch = ps.getProductByIdCate(4, 0, 20);
+        List<Product> watch = ps.getProductByIdCate(4, 0, 18);
+        System.out.println(watch.toString());
+        System.out.println("watch.size(): " + watch.size());
         request.setAttribute("watch", watch);
-        request.getRequestDispatcher("view/client/products-watch.jsp").forward(request,response);
-
+        request.getRequestDispatcher("/view/client/product-watch.jsp").forward(request, response);
     }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        doGet(request, response);
     }
 }
