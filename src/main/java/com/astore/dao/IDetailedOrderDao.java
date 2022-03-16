7c2017a463759f67b5100079546b93deade2158f
duanemellow19 <19130137@st.hcmuaf.edu.vn>
2022-03-16 13:55:29 +0700

    add order and detail order

---
+++ src/main/java/com/astore/dao/IDetailedOrderDao.java
@@ -0,0 +1,14 @@
+package com.astore.dao;
+
+import com.astore.model.DetailedOrder;
+
+import java.util.List;
+
+public interface IDetailedOrderDao {
+    public boolean insert(DetailedOrder detailedOrder);
+    public boolean delete(int id);
+    public boolean update(DetailedOrder detailedOrder);
+    public List<DetailedOrder> getAll();
+    public DetailedOrder getById(int id);
+    public List<DetailedOrder> getByNameProduct(String name);
+}
---
+++ src/main/java/com/astore/model/DetailedOrder.java
@@ -0,0 +1,70 @@
+package com.astore.model;
+
+public class DetailedOrder {
+    private int id;
+    private int idOrder;
+    private int idProduct;
+    private double priceProduct;
+    private int count ;
+    private String createAt;
+
+    public DetailedOrder(int id, int idOrder, int idProduct, double priceProduct, int count, String createAt) {
+        this.id = id;
+        this.idOrder = idOrder;
+        this.idProduct = idProduct;
+        this.priceProduct = priceProduct;
+        this.count = count;
+        this.createAt = createAt;
+    }
+
+    public DetailedOrder() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getIdOrder() {
+        return idOrder;
+    }
+
+    public void setIdOrder(int idOrder) {
+        this.idOrder = idOrder;
+    }
+
+    public int getIdProduct() {
+        return idProduct;
+    }
+
+    public void setIdProduct(int idProduct) {
+        this.idProduct = idProduct;
+    }
+
+    public double getPriceProduct() {
+        return priceProduct;
+    }
+
+    public void setPriceProduct(double priceProduct) {
+        this.priceProduct = priceProduct;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+
+    public String getCreateAt() {
+        return createAt;
+    }
+
+    public void setCreateAt(String createAt) {
+        this.createAt = createAt;
+    }
+}
